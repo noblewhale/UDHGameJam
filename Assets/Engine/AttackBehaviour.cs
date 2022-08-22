31ae7db10c515e9498990f283ec17b7995909e18
Zebadiah Long <zeb@noblewhale.com>
2022-08-22 06:44:13 -0400

    Organized everything in RogueLike Example.

--- Assets/Engine/AttackBehaviour.cs
+++ Assets/Engine/AttackBehaviour.cs
@@ -84,10 +84,10 @@ namespace Noble.TileEngine
 						case Direction.DOWN: glyph.transform.localPosition = originalPosition + Vector3.down * offset; break;
 						case Direction.RIGHT: glyph.transform.localPosition = originalPosition + Vector3.right * offset; break;
 						case Direction.LEFT: glyph.transform.localPosition = originalPosition + Vector3.left * offset; break;
-						case Direction.UP_LEFT: glyph.transform.localPosition = originalPosition + Util.UpLeft * offset; break;
-						case Direction.DOWN_LEFT: glyph.transform.localPosition = originalPosition + Util.DownLeft * offset; break;
-						case Direction.UP_RIGHT: glyph.transform.localPosition = originalPosition + Util.UpRight * offset; break;
-						case Direction.DOWN_RIGHT: glyph.transform.localPosition = originalPosition + Util.DownRight * offset; break;
+						case Direction.UP_LEFT: glyph.transform.localPosition = originalPosition + VectorUtil.UpLeft * offset; break;
+						case Direction.DOWN_LEFT: glyph.transform.localPosition = originalPosition + VectorUtil.DownLeft * offset; break;
+						case Direction.UP_RIGHT: glyph.transform.localPosition = originalPosition + VectorUtil.UpRight * offset; break;
+						case Direction.DOWN_RIGHT: glyph.transform.localPosition = originalPosition + VectorUtil.DownRight * offset; break;
 					}
 				}
 				if (attackWillHit && targetCreature)
---
+++ Assets/Examples/RogueLike/VectorUtil.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Noble.TileEngine
+{
+    using UnityEngine;
+
+    public class VectorUtil
+    {
+        public static readonly Vector3 UpLeft;
+        public static readonly Vector3 UpRight;
+        public static readonly Vector3 DownLeft;
+        public static readonly Vector3 DownRight;
+
+        static VectorUtil()
+        {
+            UpLeft = (Vector3.up + Vector3.left).normalized;
+            UpRight = (Vector3.up + Vector3.right).normalized;
+            DownLeft = (Vector3.down + Vector3.left).normalized;
+            DownRight = (Vector3.down + Vector3.right).normalized;
+        }
+
+    }
+}
