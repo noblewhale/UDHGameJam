0674b3dc6e03fab5296a8f4ea60a6bf93a9c9de9
Zebadiah Long <zeb@noblewhale.com>
2022-08-22 06:16:17 -0400

    Comments!

--- Assets/Examples/RogueLike/CreateWarpedLevelRenderTexture.cs
+++ Assets/Examples/RogueLike/CreateWarpedLevelRenderTexture.cs
@@ -9,71 +9,36 @@
         public MapRenderer render;
         void Awake()
         {
-            Vector2 pixelSize = new Vector2(Camera.main.orthographicSize * 2 * Camera.main.aspect / Screen.width, Camera.main.orthographicSize * 2 / Screen.height);
             var camera = GetComponent<Camera>();
             camera.depthTextureMode = DepthTextureMode.Depth;
 
-            // If the width is not exactly this value then you will not get pixel perfect rendering to the render texture
-            int renderTextureWidth = (int)(map.TotalWidth * 128 * 2);
+            // Times 2 for the double wide rendering that makes wrapping work
+            float renderedWidth = map.TotalWidth * 2;
+
+            // If the width is not exactly this value then we will not get pixel perfect rendering to the render texture
+            int renderTextureWidth = (int)(renderedWidth * 128);
+            // The height was chosen via experimentation such that the tiles appear squarish after circle warping
             int renderTextureHeight = (int)(renderTextureWidth / 4.5f);
 
-            var renderTexture = new RenderTexture(renderTextureWidth, renderTextureHeight, 0, RenderTextureFormat.ARGB32, 0);
+            // Create the render texture. May want to switch to HDR format here if we ever need it for effects. It does double an already very large texture size though.
+            var renderTexture = new RenderTexture(renderTextureWidth, renderTextureHeight, 0, RenderTextureFormat.ARGB32);
             renderTexture.filterMode = FilterMode.Point;
-            renderTexture.useMipMap = false;
-            renderTexture.antiAliasing = 1;
 
-            var depthTexture = new RenderTexture(renderTextureWidth, renderTextureHeight, 24, RenderTextureFormat.Depth);
+            // The depth texture is needed for the outline effect in the warp shader. It does not need to be very accurate though.
+            var depthTexture = new RenderTexture(renderTextureWidth, renderTextureHeight, 16, RenderTextureFormat.Depth);
 
-            camera.targetTexture = renderTexture;
+            // Tell the camera to render to the render and depth texture
             camera.SetTargetBuffers(renderTexture.colorBuffer, depthTexture.depthBuffer);
 
+            // Tell the render quad material to use the render and depth texture
             render.warpMaterial.mainTexture = renderTexture;
             render.warpMaterial.SetTexture("_Depth", depthTexture);
 
             // Set the camera size so that the width is 2 times the map width so wrapping magic works.
-            camera.orthographicSize = (2 * map.TotalWidth / camera.aspect) / 2.0f;
+            camera.orthographicSize = (renderedWidth / camera.aspect) / 2.0f;
 
-            float renderQuadHeight;
-            float renderQuadWidthMax = Mathf.Min(Camera.main.orthographicSize * 2 * 1.525f, Camera.main.orthographicSize * 2 * Camera.main.aspect * .8f);
-            float renderQuadWidthMin = Mathf.Min(Camera.main.orthographicSize * 2 * 1.45f, Camera.main.orthographicSize * 2 * Camera.main.aspect * .8f);
-            float w = 2 * camera.orthographicSize * 2 * camera.aspect / Mathf.PI;
-            int i = 2;
-            while (w > renderQuadWidthMax)
-            {
-                w = (2 * camera.orthographicSize * 2 * camera.aspect / Mathf.PI) / i;
-                i++;
-            }
-            float renderQuadWidth = Mathf.Max(w, renderQuadWidthMin);
-            //renderQuadWidth = (2 * camera.orthographicSize * 2 * camera.aspect / Mathf.PI) / (i - 2);
-            renderQuadWidth = pixelSize.x * (int)(renderQuadWidth / pixelSize.x);
-            renderQuadHeight = pixelSize.y * (int)(renderQuadWidth / pixelSize.y);
-            MapRenderer.instance.transform.localScale = new Vector3(renderQuadWidth, renderQuadHeight, 1);
-
-            float x, y;
-            if (MapRenderer.instance.horizontal < 0)
-            {
-                x = -MapRenderer.instance.transform.localScale.x / 2 + Camera.main.orthographicSize * Camera.main.aspect + MapRenderer.instance.horizontal;
-            }
-            else
-            {
-                x = MapRenderer.instance.transform.localScale.x / 2 - Camera.main.orthographicSize * Camera.main.aspect + MapRenderer.instance.horizontal;
-            }
-            if (MapRenderer.instance.vertical < 0)
-            {
-                y = -MapRenderer.instance.transform.localScale.y / 2 + Camera.main.orthographicSize + MapRenderer.instance.vertical;
-            }
-            else
-            {
-                y = MapRenderer.instance.transform.localScale.y / 2 - Camera.main.orthographicSize + MapRenderer.instance.vertical;
-            }
-
-            Vector3 desiredLocation = new Vector3(x, y, MapRenderer.instance.transform.localPosition.z);
-            Vector2 bottomLeft = desiredLocation - MapRenderer.instance.transform.localScale / 2;
-            bottomLeft.x = pixelSize.x * (int)(bottomLeft.x / pixelSize.x);
-            bottomLeft.y = pixelSize.y * (int)(bottomLeft.y / pixelSize.y);
-            desiredLocation = bottomLeft + (Vector2)MapRenderer.instance.transform.localScale / 2;
-            desiredLocation.z = MapRenderer.instance.transform.localPosition.z;
-            MapRenderer.instance.transform.localPosition = desiredLocation;
+            // Now that the camera is set up we can place the render quad
+            MapRenderer.instance.PlaceQuad();
         }
     }
 }
