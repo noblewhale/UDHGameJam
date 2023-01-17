6b108be83eb3f6c587be2cd388f3920ed8a703b6
Zebadiah Long <zeb@noblewhale.com>
2023-01-16 21:55:35 -0500

    named a local variable better for easier searching

--- Assets/Engine/AttackBehaviour.cs
+++ Assets/Engine/AttackBehaviour.cs
@@ -80,19 +80,19 @@ namespace Noble.TileEngine
 
 				float offset = identityCreature.attackMovementAnimation.Evaluate(identityCreature.attackAnimationTime / identityCreature.attackAnimationDuration) * identityCreature.attackAnimationScale;
 
-				var glyph = identityCreature.baseObject.glyphs;
-				if (glyph)
+				var glyphs = identityCreature.baseObject.glyphs;
+				if (glyphs)
 				{
 					switch (identityCreature.lastDirectionAttackedOrMoved)
 					{
-						case Direction.UP: glyph.transform.localPosition = originalPosition + Vector3.up * offset; break;
-						case Direction.DOWN: glyph.transform.localPosition = originalPosition + Vector3.down * offset; break;
-						case Direction.RIGHT: glyph.transform.localPosition = originalPosition + Vector3.right * offset; break;
-						case Direction.LEFT: glyph.transform.localPosition = originalPosition + Vector3.left * offset; break;
-						case Direction.UP_LEFT: glyph.transform.localPosition = originalPosition + VectorUtil.UpLeft * offset; break;
-						case Direction.DOWN_LEFT: glyph.transform.localPosition = originalPosition + VectorUtil.DownLeft * offset; break;
-						case Direction.UP_RIGHT: glyph.transform.localPosition = originalPosition + VectorUtil.UpRight * offset; break;
-						case Direction.DOWN_RIGHT: glyph.transform.localPosition = originalPosition + VectorUtil.DownRight * offset; break;
+						case Direction.UP: glyphs.transform.localPosition = originalPosition + Vector3.up * offset; break;
+						case Direction.DOWN: glyphs.transform.localPosition = originalPosition + Vector3.down * offset; break;
+						case Direction.RIGHT: glyphs.transform.localPosition = originalPosition + Vector3.right * offset; break;
+						case Direction.LEFT: glyphs.transform.localPosition = originalPosition + Vector3.left * offset; break;
+						case Direction.UP_LEFT: glyphs.transform.localPosition = originalPosition + VectorUtil.UpLeft * offset; break;
+						case Direction.DOWN_LEFT: glyphs.transform.localPosition = originalPosition + VectorUtil.DownLeft * offset; break;
+						case Direction.UP_RIGHT: glyphs.transform.localPosition = originalPosition + VectorUtil.UpRight * offset; break;
+						case Direction.DOWN_RIGHT: glyphs.transform.localPosition = originalPosition + VectorUtil.DownRight * offset; break;
 					}
 				}
 				if (attackWillHit && targetCreature)
