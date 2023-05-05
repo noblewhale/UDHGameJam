9fd5d2fffd63298ca8c2a3109e4ff089bf4220fa
Zebadiah Long <zeb@noblewhale.com>
2023-05-04 21:56:38 -0400

    Fixed issue with pushing boulder into collidables. Made everything start revealed in sokoban level

--- Assets/Boulder.cs
+++ Assets/Boulder.cs
@@ -13,37 +13,40 @@ public class Boulder : MonoBehaviour
 
     public void PushBoulder(DungeonObject ob, bool isInstigator)
     {
-        //reference to the boulder dungeon object
-        DungeonObject baseObject = GetComponent<DungeonObject>();
-
-        //Reference to boulder position 
-        var boulderPosition = baseObject.tilePosition;
-
-        //Reference to position of the thing pushing the boulder
-        var pusherPosition = ob.tilePosition;
-
-       
-        var pushDirection = boulderPosition - pusherPosition;
-
-        var updatedBoulderPosition = boulderPosition + pushDirection;
-
-        Map.instance.TryMoveObject(baseObject, updatedBoulderPosition);
-
-        var potentialNewTile = Map.instance.GetTile(updatedBoulderPosition);
-
-        if (potentialNewTile.ContainsObjectOfType("Water") || potentialNewTile.ContainsObjectOfType("Acid"))
+        if (!isInstigator)
         {
-            var waterToDestroy = potentialNewTile.GetObjectOfType("Water");
-            var acidToDestroy = potentialNewTile.GetObjectOfType("Acid");
-            
-            
-            potentialNewTile.RemoveObject(waterToDestroy, true);
-           
-            potentialNewTile.RemoveObject(acidToDestroy, true);
-            
-            potentialNewTile.RemoveObject(baseObject, true);
-        }
+            //reference to the boulder dungeon object
+            DungeonObject baseObject = GetComponent<DungeonObject>();
 
-        Debug.Log("pushing");
+            //Reference to boulder position 
+            var boulderPosition = baseObject.tilePosition;
+
+            //Reference to position of the thing pushing the boulder
+            var pusherPosition = ob.tilePosition;
+
+            var pushDirection = boulderPosition - pusherPosition;
+
+            var updatedBoulderPosition = boulderPosition + pushDirection;
+
+            var potentialNewTile = Map.instance.GetTile(updatedBoulderPosition);
+
+            if (!potentialNewTile.IsCollidable())
+            {
+                Map.instance.MoveObject(baseObject, updatedBoulderPosition);
+
+                if (potentialNewTile.ContainsObjectOfType("Water") || potentialNewTile.ContainsObjectOfType("Acid"))
+                {
+                    var waterToDestroy = potentialNewTile.GetObjectOfType("Water");
+                    var acidToDestroy = potentialNewTile.GetObjectOfType("Acid");
+
+                    potentialNewTile.RemoveObject(waterToDestroy, true);
+                    potentialNewTile.RemoveObject(acidToDestroy, true);
+
+                    potentialNewTile.RemoveObject(baseObject, true);
+                }
+
+                Debug.Log("pushing");
+            }
+        }
     }
 }
