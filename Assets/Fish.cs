dc72ba0dcaa3ccf2e595382fbf35022178c46f0c
ferraribaw <diga.brent@gmail.com>
2023-06-01 20:23:36 -0400

    same 3

--- Assets/Fish.cs
+++ Assets/Fish.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    DungeonObject threatenedFish;
+    Tile threatenedFishTile;
+    List<Tile> adjacentWater = new();
 
     public void FishCrush(DungeonObject stepper)
     {
+        threatenedFish = GetComponent<DungeonObject>();
+        threatenedFishTile = threatenedFish.tile;
+        adjacentWater.Clear();
 
-        if (stepper.objectName == "Boulder")
+        GetAdjacentWaterTile(-1,0);
+        GetAdjacentWaterTile(1, 0);
+        GetAdjacentWaterTile(0, -1);
+        GetAdjacentWaterTile(0, 1);
+
+        if (adjacentWater.Count == 0)
         {
+            if (stepper.objectName == "Boulder")
+            {
+                threatenedFishTile.RemoveObject(threatenedFish, true);
+            }
+        }
+        else
+        {
+            var possibleWaterDestination = UnityEngine.Random.Range(0,adjacentWater.Count);
 
-
-            var threatenedFish = GetComponent<DungeonObject>();
-            var threatenedFishTile = threatenedFish.tile;
-
-            threatenedFishTile.RemoveObject(threatenedFish, true);
-
+            //Map.instance.MoveObject(threatenedFish, adjacentWater[possibleWaterDestination].tilePosition);
         }
 
+        
+    }
 
+    void GetAdjacentWaterTile(int x, int y)
+    {
+        var adjacentTile = Map.instance.GetTile(threatenedFishTile.x + x, threatenedFishTile.y + y);
+
+        if (adjacentTile.ContainsObjectOfType("Water"))
+        {
+            adjacentWater.Add(adjacentTile);
+        }
     }
 
 
